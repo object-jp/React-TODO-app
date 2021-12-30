4ddd022d5054547b3f64cf3e2c1e145110fb6dfe
object <83064497+object-jp@users.noreply.github.com>
2021-12-30 21:13:49 +0900

    未完了項目を5個までに制限

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,11 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>登録できるtodoは5個までだと余！</p>
+      )}
       <IncompleteTodo
         incomplete={incompleteTodos}
         onClickComplete={onClickComplete}
